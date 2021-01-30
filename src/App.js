3d7658cb67a13372fa0b8148f62edc0439f8f7d8
root <dcdev001@gmail.com>
2021-01-30 00:50:13 +0000

    Show/Hide Screenshot

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import Feedback from "./feedback";
+import "./App.css";
 
 class App extends Component {
 	render() {
 		return (
-			<Feedback />
+			<div className="App">
+				<Feedback />
+			</div>
 		)
 	}
 }
--- src/feedback.js
+++ src/feedback.js
@@ -4,6 +4,13 @@ import html2canvas from 'html2canvas';
 import $ from 'jquery'; 
 
 class feedback extends Component {
+	handleChange() {
+		if($('.form-check-input').is(":checked"))   
+            $("#show").show();
+        else
+            $("#show").hide();
+	}
+	
 	handleClick() {
 		const input = $('body')[0];
 		console.log(input)
@@ -16,8 +23,11 @@ class feedback extends Component {
 
 				var newImg = document.createElement("img"); // create img tag
 			    newImg.src = url;
-				console.log($("#show"));
+				// console.log($("#show"));
 			    $("#show").html(newImg); 
+				// console.log($("#show").children("img"));
+				$("#show").children("img").addClass("img-width");
+				$("#show").hide();
 			});
 	}
 	
@@ -34,8 +44,23 @@ class feedback extends Component {
 						  <span aria-hidden="true">&times;</span>
 						</button>
 					  </div>
-					  <div id="show" className="modal-body">
-						...
+					  <div className="modal-body">
+						  <div className="modal-body-text">
+							  Have Feedback? We'd love to hear it, but please dont't share sensitive information. Have questions? Try help or support.
+						  </div>
+						  <div className="form-check">
+							  <input 
+								  onChange={this.handleChange}
+								  className="form-check-input" 
+								  type="checkbox" 
+								  value="" 
+								  id="flexSwitchCheckDefault"
+							  />
+							  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+								Include Screenshot
+							  </label>
+						  </div>
+						  <div id="show"></div>
 					  </div>
 					  <div className="modal-footer">
 						<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
