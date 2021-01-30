c69f9966b27993f8676ef7b7cc9d59c6df1a5dbc
root <dcdev001@gmail.com>
2021-01-30 07:11:12 +0000

    Minor Changes

--- src/feedback.js
+++ src/feedback.js
@@ -26,8 +26,6 @@ class feedback extends Component {
   };
 
   onCropChange = (crop, percentCrop) => {
-    // You could also use percentCrop:
-    // this.setState({ crop: percentCrop });
     this.setState({ crop });
   };
 
@@ -67,8 +65,7 @@ class feedback extends Component {
     return new Promise((resolve, reject) => {
       canvas.toBlob(blob => {
         if (!blob) {
-          //reject(new Error('Canvas is empty'));
-          console.error('Canvas is empty');
+          // console.error('Canvas is empty');
           return;
         }
         blob.name = fileName;
@@ -79,6 +76,19 @@ class feedback extends Component {
     });
   }
 
+	handleUpdate() {
+		let fback = $('textarea#feedbackText').val();
+		data.message = fback;
+		
+		
+		if($('.form-check-input').is(":checked")) {
+			data.imgData = $("#crop1").attr("src");
+		} 
+		else {
+			data.imgData = "";
+		}
+	}
+
 	handleChange() {
 		if($('.form-check-input').is(":checked"))   
             $("#show").show();
@@ -87,27 +97,7 @@ class feedback extends Component {
 	}
 	
 	handleSubmitClick() {
-		let fback = $('textarea#feedbackText').val();
-		data.message = fback;
-		
-		// console.log($("#crop1").src);
-		// console.log("hi");
-		
-		
-		// const input1 = $("#crop1")[0];
-		// html2canvas(input1)
-		// 	.then((canvas) => {
-		// 		url1 = canvas.toDataURL();
-		// 		// console.log(url1);
-		// 	});
-		// console.log(url1);
-		if($('.form-check-input').is(":checked")) {
-			data.imgData = $("#crop1").attr("src");
-		} 
-		else {
-			data.imgData = "";
-		}
-		console.log(data);
+		console.log(data)
 		makereq();
 		
 	}
@@ -118,26 +108,20 @@ class feedback extends Component {
 
 		html2canvas(input)
 			.then((canvas) => {
-				// const imgData = canvas.toDataURL('image/png');
-				// console.log(imgData);
 				url = canvas.toDataURL();
-				// console.log(url);
-				//var newImg = document.createElement("img"); // create img tag
-			    //newImg.src = url;
-				console.log($(".ReactCrop__image").attr("src", url));
-			    //$("#show").html(newImg); 
-				// console.log($("#show").children("img"));
-				//$("#show").children("img").addClass("img-width");
+				$(".ReactCrop__image").attr("src", url);
 				$("#show").hide();
 			});
+		console.log(url);
+		// $(".ReactCrop__image").attr("src", url);
 	}
 	
 	render() {
 		const { crop, croppedImageUrl } = this.state;
-		console.log(croppedImageUrl);
+		// console.log(croppedImageUrl);
 		return (
 			<div className="container text-center">
-			  <button onClick={this.handleClick} type="button" className="btn btn-primary" data-toggle="modal" data-target="#myModal">Send Feedback</button>
+			  <button onClick={this.handleClick} type="button" className="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Send Feedback</button>
 			  <div className="modal fade" id="myModal" data-backdrop="static" data-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" role="dialog">
 				<div className="modal-dialog">
 					<div className="modal-content">
@@ -164,6 +148,7 @@ class feedback extends Component {
 							  </label>
 						  </div>
 						  <div id="show">
+							  <div>Original Screenshot</div>
 							  <ReactCrop
 								src={url}
 								crop={crop}
@@ -172,11 +157,12 @@ class feedback extends Component {
 								onComplete={this.onCropComplete}
 								onChange={this.onCropChange}
 							  />
-							  <img id="crop1" alt="Crop" style={{ maxWidth: '100%' }} src={croppedImageUrl} />
+							  <div>Cropped Screenshot</div>
+							  <img id="crop1" alt="url" style={{ maxWidth: '100%' }} src={croppedImageUrl || url} />
 						  </div>
 					  </div>
 					  <div className="modal-footer">
-						<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+						<button onClick={this.handleUpdate} type="button" className="btn btn-secondary" >Update</button>
 						<button onClick={this.handleSubmitClick} type="button" className="btn btn-primary">Submit</button>
 					  </div>
 					</div>
@@ -188,15 +174,22 @@ class feedback extends Component {
 }
 
 function makereq() {
-	fetch("https://reqbin.com/echo/post/json", {
-	  method: "POST",
-	  headers: [
-		["Content-Type", "application/json"],
-		["Content-Type", "text/plain"]
-	  ],
-	  credentials: "include",
-	  body: JSON.stringify(data)
-	});
+	var url = "https://reqbin.com/echo/post/json";
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url);
+
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.setRequestHeader("Content-Type", "application/json");
+
+	xhr.onreadystatechange = function () {
+	   if (xhr.readyState === 4) {
+		  console.log(xhr.status);
+		  console.log(xhr.responseText);
+	   }};
+
+
+	xhr.send(JSON.stringify(data));
 	alert("Feedback Submitted");
 }
 
